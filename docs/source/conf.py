c74b5f3a209323b1e0cf0cc8130eb78b7f4bd1d8
Bryan Herman <bherman@mit.edu>
2011-10-23 10:41:22 -0400

    added CMFD logo to html page

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -92,26 +92,27 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # html_theme = 'default'
-html_theme = 'sphinxdoc'
+html_theme = 'haiku'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {'nosidebar': 'true',
+                      'full_logo': 'true'}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "CMFD Documentation" 
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = "CMFD Documentation" 
+# html_short_title = "CMFD Documentation" 
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = './pics/CMFDlogo.png' 
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
