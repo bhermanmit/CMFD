201293a69a8abad80d9be7ab0ad14e1f48c43bb3
Bryan Herman <bherman@kilkenny.bw01.mit.edu>
2012-02-20 11:25:30 -0800

    created an initialize and finalize routine in main

--- src/main.F90
+++ src/main.F90
@@ -10,19 +10,6 @@ program main
 #include <finclude/slepcsys.h>
 #include <finclude/slepceps.h>
 
-  ! initialize slepc/petsc
-  call SlepcInitialize(PETSC_NULL_CHARACTER,ierr)
-
-  ! get mpi info
-  call MPI_COMM_RANK(PETSC_COMM_WORLD,rank,ierr)
-  call MPI_COMM_SIZE(PETSC_COMM_WORLD,n_procs,ierr)
-
-  ! find master
-  if (rank == 0) master = .true.
-
-  ! print out number of procs
-  if (master) print *,'Number of Processors:',n_procs
-
   ! begin total timer
   call timer_start(time_total)
 
@@ -32,11 +19,44 @@ program main
   ! stop timer
   call timer_stop(time_total)
 
-  ! finalize slepc/petsc
-  call SlepcFinalize(ierr)
-  
-  ! print output
-  print *,"Total Execution time (s):",time_total%elapsed
-  print *,"K-effective:",cmfd%keff
+contains
+
+!==============================================================================
+! INITIALIZE
+!==============================================================================
+
+  subroutine initialize()
+
+    ! initialize slepc/petsc
+    call SlepcInitialize(PETSC_NULL_CHARACTER,ierr)
+
+    ! get mpi info
+    call MPI_COMM_RANK(PETSC_COMM_WORLD,rank,ierr)
+    call MPI_COMM_SIZE(PETSC_COMM_WORLD,n_procs,ierr)
+
+    ! find master
+    if (rank == 0) master = .true.
+
+    ! print out number of procs
+    if (master) print *,'Number of Processors:',n_procs
+
+  end subroutine initialize
+
+!==============================================================================
+! FINALIZE
+!==============================================================================
+
+  subroutine finalize()
+
+    ! finalize slepc/petsc
+    call SlepcFinalize(ierr)
+
+    ! print output
+    if (master) then
+      print *,"Total Execution time (s):",time_total%elapsed
+      print *,"K-effective:",cmfd%keff
+    end if
+
+  end subroutine finalize
 
 end program main
